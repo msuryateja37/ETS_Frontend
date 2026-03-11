151b017d1704273801af1871e1f0eec546f13d9d
Surya Teja <suryateja@Suryas-mini.local>
2026-03-12 00:50:02 +0530

    yml fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,5 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  // reactCompiler: true,
-  experimental: {
-    appDir: true,
-  },
   output: "standalone"
-};
+}
 
-export default nextConfig;
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {
-//     appDir: true
-//   }
-// }
-
-// export default nextConfig
+export default nextConfig
